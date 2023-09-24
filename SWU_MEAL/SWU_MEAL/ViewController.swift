3107d8f9581a5089a68cbf5de7bce38ae1f0779d
AlwaysFighting <zosejiri@gmail.com>
2023-09-24 17:33:20 +0900

    init commit

---
+++ SWU_MEAL/SWU_MEAL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SWU_MEAL
+//
+//  Created by 목정아 on 2023/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
