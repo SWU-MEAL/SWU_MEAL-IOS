5dd20956fa4c6b5405209d606c72326bc4c35f3a
AlwaysFighting <zosejiri@gmail.com>
2023-09-27 15:20:38 +0900

    Feat: 마이페이지 완

--- SWU_MEAL/SWU_MEAL/Extension+/Extension+.swift
+++ SWU_MEAL/SWU_MEAL/Extension+/Extension+.swift
@@ -93,6 +93,18 @@ extension UIView{
             corners.contains(.bottomLeft) ? .layerMinXMaxYCorner:.init(),
         ]
     }
-    
 }
 
+// MARK: - UIApplication
+
+extension UIApplication {
+    static var release: String {
+        return Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String? ?? "x.x"
+    }
+    static var build: String {
+        return Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as! String? ?? "x"
+    }
+    static var version: String {
+        return "\(release).\(build)"
+    }
+}
---
+++ SWU_MEAL/SWU_MEAL/마이페이지/MypageModel.swift
@@ -0,0 +1,12 @@
+//
+//  MypageModel.swift
+//  SWU_MEAL
+//
+//  Created by 목정아 on 2023/09/26.
+//
+
+import Foundation
+
+struct MypageModel {
+    let title: String
+}
